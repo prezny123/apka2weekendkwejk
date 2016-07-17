7b91458ac00425e8e53a4d1d82daf8c6cafce24b
Krzysztof Pawlak <krzys.pawlak@wp.pl>
2016-07-17 10:49:12 +0000

    added controller home

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  root 'home#index'
+  
+
   devise_for :users
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   # The priority is based upon order of creation: first created -> highest priority.
