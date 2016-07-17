f07f2d1a344c3701c164840a01c269aa6cb95f92
Krzysztof Pawlak <krzys.pawlak@wp.pl>
2016-07-17 14:10:08 +0000

    final commit

--- app/controllers/mems_controller.rb
+++ app/controllers/mems_controller.rb
@@ -1,11 +1,22 @@
 class MemsController < ApplicationController
   before_action :set_mem, only: [:show, :edit, :update, :destroy]
-
+  before_action :authenticate_user!, only: [:create,:my]
   # GET /mems
   # GET /mems.json
   def index
-    @mems = Mem.all
+  @mems = Mem.active
   end
+  
+  def my
+ @mems = current_user.mems
+ render :index
+ end
+ 
+ def inactive
+ @mems = Mem.inactive
+ render :index
+ end
+
 
   # GET /mems/1
   # GET /mems/1.json
@@ -24,7 +35,9 @@ class MemsController < ApplicationController
   # POST /mems
   # POST /mems.json
   def create
-    @mem = Mem.new(mem_params)
+  
+    @mem = current_user.mems.new(mem_params)
+
 
     respond_to do |format|
       if @mem.save
--- app/models/mem.rb
+++ app/models/mem.rb
@@ -2,8 +2,13 @@ class Mem < ActiveRecord::Base
   belongs_to :user
   has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment :image,:content_type => {:content_type => ["image/jpg", "image/gif", "image/png", "image/jpeg"]}
+
+  validates :name, presence: true
+  
   after_validation(on: :create) do
-      self.active = false
-      validates :name, presence: true
+    self.active = false
   end
+  scope :active, ->{where active: true}
+  scope :inactive, ->{where active: false}
+
 end
